ffce5aa9a39d52199d076e688715853ba8415332
Mareenbck <marine.bascunana@gmail.com>
2022-11-15 17:21:40 +0100

    en cours de test

--- includes/containers/vector.hpp
+++ includes/containers/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: mbascuna <mbascuna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/11 14:47:39 by mbascuna          #+#    #+#             */
-/*   Updated: 2022/11/15 10:45:27 by mbascuna         ###   ########.fr       */
+/*   Updated: 2022/11/15 17:19:55 by mbascuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -224,8 +224,11 @@ class vector
 
 		void pop_back(void)
 		{
-			_alloc.destroy(&back());
-			_size--;
+			// if (size() > 0)
+			// {
+				_alloc.destroy(&back());
+				_size--;
+			// }
 		}
 
 		iterator insert(iterator position, const T& x)
@@ -235,7 +238,7 @@ class vector
 			{
 				size_type i = 0;
 				size_type newSize = _size + 1;
-				pointer newArr = _alloc.allocate(size() + 1);
+				pointer newArr = _alloc.allocate(capacity() * 2);
 				for (; i < pos; i++)
 					_alloc.construct(&newArr[i], _arr[i]);
 				_alloc.construct(&newArr[pos], x);
@@ -246,7 +249,7 @@ class vector
 				_alloc.deallocate(_arr, capacity());
 				_arr = newArr;
 				_size = newSize;
-				_capacity = newSize;
+				_capacity = capacity() * 2;
 				return (pos + begin());
 			}
 			if (!empty())
@@ -315,6 +318,18 @@ class vector
 		template <class InputIterator>
 		void insert(iterator position, InputIterator first, InputIterator last,  typename ft::enable_if<!ft::is_integral<InputIterator>::value, InputIterator>::type* = 0)
 		{
+			// size_type i = last - first;
+			// if (i + size() >= _capacity)
+			// {
+			// 	size_type newCap = _size + i;
+			// 	if (newCap <= (2 * _capacity))
+			// 	{
+			// 		newCap = 2 * _size;
+			// 		if (newCap < size() + i)
+			// 			newCap = size() + i;
+			// 	}
+			// 	_capacity = newCap;
+			// }
 			for (InputIterator it = first; it != last; it++)
 			{
 				position = insert(position, *it);
--- testing/vector_test.cpp
+++ testing/vector_test.cpp
@@ -6,7 +6,7 @@
 /*   By: mbascuna <mbascuna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/15 14:36:54 by mbascuna          #+#    #+#             */
-/*   Updated: 2022/11/15 13:15:24 by mbascuna         ###   ########.fr       */
+/*   Updated: 2022/11/15 17:19:03 by mbascuna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,32 +37,32 @@ void print_vector(NS::vector<std::string> &vct)
 
 void check_constructor(void)
 {
-	std::cout << YELLOW << "▶️​​  CONSTRUCTORS TESTS " << RESET << std::endl << std::endl;
-	std::cout << " " << GREEN << " | " << UL << "default:" << RESET << " vector();" << std::endl;
+	std::cout << PINK << ULL << "▶️​​  CONSTRUCTORS TESTS " << RESET << std::endl << std::endl;
+	std::cout << " " << YELLDARK << BOLD  << " | " << UL << "default:" << RESET << " vector();" << std::endl;
 	{
 		NS::vector<int> vec;
 		print_vector(vec);
 	}
-	std::cout << " " << GREEN << " | " << UL << "with size:" << RESET << " vector(5);" << std::endl;
+	std::cout << " " << YELLDARK << BOLD  << " | " << UL << "with size:" << RESET << " vector(5);" << std::endl;
 	{
 		NS::vector<int> vec(5);
 		print_vector(vec);
 	}
-	std::cout << " " << GREEN << " | " << UL << "with size + value:" << RESET << " vector(5, 42);" << std::endl;
+	std::cout << " " << YELLDARK << BOLD  << " | " << UL << "with size + value:" << RESET << " vector(5, 42);" << std::endl;
 	{
 		NS::vector<int> vec(5, 42);
 		print_vector(vec);
-		std::cout << " " << GREEN << " | " << UL << "with iterator:" << RESET << " vector(vec.begin(), vec.end());" << std::endl;
+		std::cout << " " << YELLDARK << BOLD  << " | " << UL << "with iterator:" << RESET << " vector(vec.begin(), vec.end());" << std::endl;
 		NS::vector<int> vec1(vec.begin(), vec.end());
 		print_vector(vec1);
-		std::cout << " " << GREEN << " | " << UL << "with range:" << RESET << " vct_range(it, --(--ite));" << std::endl;
+		std::cout << " " << YELLDARK << BOLD  << " | " << UL << "with range:" << RESET << " vct_range(it, --(--ite));" << std::endl;
 		NS::vector<int>::iterator it = vec.begin(), ite = vec.end();
 		NS::vector<int> vct_range(it, --(--ite));
 		print_vector(vct_range);
-		std::cout << " " << GREEN << " | " << UL << "copy:" << RESET << " vct_copy(vct);" << std::endl;
+		std::cout << " " << YELLDARK << BOLD  << " | " << UL << "copy:" << RESET << " vct_copy(vct);" << std::endl;
 		NS::vector<int> vct_copy(vec);
 		print_vector(vct_copy);
-		std::cout << " " << GREEN << " | " << UL << "operator = " << RESET << " vct_range = vec;" << std::endl;
+		std::cout << " " << YELLDARK << BOLD  << " | " << UL << "operator = " << RESET << " vct_range = vec;" << std::endl;
 		vct_range = vec;
 		print_vector(vct_range);
 	}
@@ -70,14 +70,14 @@ void check_constructor(void)
 
 void ft_check_assign(void)
 {
-	std::cout << YELLOW << "▶️​  ASSIGN TESTS " << RESET << std::endl;
+	std::cout << PINK << ULL << "▶️​  ASSIGN TESTS " << RESET << std::endl;
 
-	std::cout << " " << GREEN << " | " << UL << "assign:" << RESET << " vct(4, /"" Ola !/"");" << std::endl;
+	std::cout << " " << YELLDARK << BOLD << " | " << UL << "assign:" << RESET << " vct(4, /"" Ola !/"");" << std::endl;
 	NS::vector<std::string> vec3;
 	vec3.assign(4, "Ola !");
 	print_vector(vec3);
 
-	std::cout << " " << GREEN << " | " << UL << "assign w/ iterator:" << RESET << " vec1.assign(lst.begin(), lst.end());" << std::endl;
+	std::cout << " " << YELLDARK << BOLD << " | " << UL << "assign w/ iterator:" << RESET << " vec1.assign(lst.begin(), lst.end());" << std::endl;
 	NS::vector<int> vec1;
 	std::list<int> lst;
 	for (int i = 0; i < 10; i++)
@@ -92,44 +92,44 @@ void ft_check_assign(void)
 
 void check_capacity(void)
 {
-	std::cout << YELLOW << "▶️​​  CAPACITY TESTS " << RESET << std::endl;
-	std::cout << " " << GREEN << " | " << UL << "size empty:" << RESET << " vector()" << std::endl;
+	std::cout << PINK << ULL << "▶️​​  CAPACITY TESTS " << RESET << std::endl;
+	std::cout << " " << YELLDARK << BOLD << " | " << UL << "size empty:" << RESET << " vector()" << std::endl;
 	NS::vector<int> vct;
 	std::cout << "   size : " << vct.size() << " | capacity : " << vct.capacity() << " | max_size : " << vct.max_size() << std::endl << std::endl;
-	std::cout << " " << GREEN << " | " << UL << "size 10000:" << RESET << " vct1(10000)" << std::endl;
+	std::cout << " " << YELLDARK << BOLD << " | " << UL << "size 10000:" << RESET << " vct1(10000)" << std::endl;
 	NS::vector<int> vct1(10000);
 	std::cout << "   size : " << vct1.size() << " | capacity : " << vct1.capacity() << " | max_size : " << vct1.max_size() << std::endl << std::endl;
-	std::cout << " " << GREEN << " | " << UL << "size 1:" << RESET << " vct2(1)" << std::endl;
+	std::cout << " " << YELLDARK << BOLD << " | " << UL << "size 1:" << RESET << " vct2(1)" << std::endl;
 	NS::vector<int> vct2(1);
 	std::cout << "   size : " << vct2.size() << " | capacity : " << vct2.capacity() << " | max_size : " << vct2.max_size() << std::endl << std::endl;
 
-	std::cout << YELLOW << "​▶️​​  RESIZE TESTS " << RESET << std::endl;
-	std::cout << " " << GREEN << " | " << UL << "resize():" << RESET << " vct1.resize(5, 42)" << std::endl;
+	std::cout << PINK << ULL << "​▶️​​  RESIZE TESTS " << RESET << std::endl;
+	std::cout << " " << YELLDARK << BOLD << " | " << UL << "resize():" << RESET << " vct1.resize(5, 42)" << std::endl;
 	vct1.resize(5, 42);
 	print_vector(vct1);
-	std::cout << " " << GREEN << " | " << UL << "resize():" << RESET << " vct.resize(42)" << std::endl;
+	std::cout << " " << YELLDARK << BOLD << " | " << UL << "resize():" << RESET << " vct.resize(42)" << std::endl;
 	vct.resize(42);
 	print_vector(vct);
-	std::cout << " " << GREEN << " | " << UL << "resize():" << RESET << " vct2.resize(0)" << std::endl;
+	std::cout << " " << YELLDARK << BOLD << " | " << UL << "resize():" << RESET << " vct2.resize(0)" << std::endl;
 	vct2.resize(0);
 	print_vector(vct2);
 
-	std::cout << YELLOW << "▶️​​  EMPTY TESTS " << RESET << std::endl;
-	std::cout << " " << GREEN << " | " << UL << "vct:" << RESET << " " << vct.size() << std::endl;
+	std::cout << PINK << ULL << "▶️​​  EMPTY TESTS " << RESET << std::endl;
+	std::cout << " " << YELLDARK << BOLD << " | " << UL << "vct:" << RESET << " " << vct.size() << std::endl;
 	std::cout << "   bool :" << vct.empty() << std::endl;
-	std::cout << " " << GREEN << " | " << UL << "vct1:" << RESET << " " << vct1.size() << std::endl;
+	std::cout << " " << YELLDARK << BOLD << " | " << UL << "vct1:" << RESET << " " << vct1.size() << std::endl;
 	std::cout << "   bool :" << vct1.empty() << std::endl;
-	std::cout << " " << GREEN << " | " << UL << "vct2:" << RESET << " " << vct2.size() << std::endl;
+	std::cout << " " << YELLDARK << BOLD << " | " << UL << "vct2:" << RESET << " " << vct2.size() << std::endl;
 	std::cout << "   bool :" << vct2.empty() << std::endl << std::endl;
 
-	std::cout << YELLOW << "▶️​​  RESERVE TESTS " << RESET << std::endl;
-	std::cout << " " << GREEN << " | " << UL << "reserve:" << RESET << " vct.reserve(10)" << std::endl;
+	std::cout << PINK << ULL << "▶️​​  RESERVE TESTS " << RESET << std::endl;
+	std::cout << " " << YELLDARK << BOLD << " | " << UL << "reserve:" << RESET << " vct.reserve(10)" << std::endl;
 	vct.reserve(10);
 	print_vector(vct);
-	std::cout << " " << GREEN << " | " << UL << "reserve:" << RESET << " vct1.reserve(100)" << std::endl;
+	std::cout << " " << YELLDARK << BOLD << " | " << UL << "reserve:" << RESET << " vct1.reserve(100)" << std::endl;
 	vct1.reserve(100);
 	print_vector(vct1);
-	std::cout << " " << GREEN << " | " << UL << "reserve:" << RESET << " vct2.reserve(0)" << std::endl;
+	std::cout << " " << YELLDARK << BOLD << " | " << UL << "reserve:" << RESET << " vct2.reserve(0)" << std::endl;
 	vct2.reserve(100);
 	print_vector(vct2);
 }
@@ -201,65 +201,29 @@ void check_resize_ref(void)
 
 /*********************************MODIFIERS************************************/
 
-
-
 void ft_check_pushback(void)
 {
 	NS::vector<int> vct;
-	// int myint;
-
-	// std::cout << "Please enter some integers (enter 0 to end):\n";
-	myvector.push_back(1);
-	myvector.push_back(2);
-	myvector.push_back(3);
-	myvector.pop_back();
-
-	std::cout << "myvector stores " << int(myvector.size()) << " numbers.\n";
-	for (int i = 0; i < int(myvector.size()); i++)
-		std::cout << myvector[i] << " ";
-	std::cout << std::endl;
-
-	NS::vector<int> vct;
-	std::cout << YELLOW << "▶️​  PUSH_BACK TESTS " << RESET << std::endl;
-	std::cout << " " << GREEN << " | " << UL << "push_back:" << RESET << " vct.push_back(42)" << std::endl;
+	std::cout << PINK << ULL << "▶️​  PUSH_BACK TESTS " << RESET << std::endl;
+	std::cout << YELLDARK << BOLD << "  | " << UL << "push_back:" << RESET << " vct.push_back(42)" << std::endl;
 	vct.push_back(42);
 	print_vector(vct);
-	std::cout << " " << GREEN << " | " << UL << "push_back in loop:" << RESET << " vct.push_back(54)" << std::endl;
+	std::cout << YELLDARK << BOLD << "  | " << UL << "push_back in loop:" << RESET << " vct.push_back(54)" << std::endl;
 		for (int i = 0; i < 3;  i++)
 			vct.push_back(54);
 	print_vector(vct);
 
-	std::cout << YELLOW << "▶️​  POP_BACK TESTS " << RESET << std::endl;
-
-	// std::cout << SPACE << BOLD << ORANGE << UL << "--> push_back():" << RESET << " vec1.push_back();" << std::endl << std::endl;
-	// 	std::cout << TAB << "-> push_back 42 in vec1 : vec1.push_back(42);" << std::endl;
-	// 	vec1.push_back(42);
-	// 	printVec(vec1, 1);
-	// 	std::cout << TAB << "-> push_back 3 times 42 in vec1 : vec1.push_back(42);" << std::endl;
-	// 	for (int i = 0; i < 3;  i++)
-	// 		vec1.push_back(42);
-	// 	printVec(vec1, 1);
-	// 	std::cout << TAB << "-> push_back 42 in an empty vector : vec2.push_back(42);" << std::endl;
-	// 	{
-	// 		NS::vector<T1> vec2;
-	// 		vec2.push_back(42);
-	// 		printVec(vec2, 1);
-	// 	}
-	// 	std::cout << SPACE << BOLD << ORANGE << UL << "--> pop_back():" << RESET << " vec1.pop_back();" << std::endl << std::endl;
-	// 	std::cout << TAB << "-> pop_back vec1 : vec1.pop_back();" << std::endl;
-	// 	vec1.pop_back();
-	// 	printVec(vec1, 1);
-	// 	std::cout << TAB << "-> pop_back 3 times vec1 : vec1.pop_back();" << std::endl;
-	// 	for (int i = 0; i < 3;  i++)
-	// 		vec1.pop_back();
-	// 	printVec(vec1, 1);
-	// 	std::cout << TAB << "-> pop_back 4 times vec1 : vec1.pop_back();" << std::endl;
-	// 	for (int i = 0; i < 4;  i++)
-	// 		vec1.pop_back();
-	// 	printVec(vec1, 1);
+	std::cout << PINK << ULL << "▶️​  POP_BACK TESTS " << RESET << std::endl;
+	std::cout << YELLDARK << BOLD << "  | " << UL << "pop_bakc" << RESET << " vct.pop_back()" << std::endl;
+	vct.pop_back();
+	print_vector(vct);
+	std::cout << YELLDARK << BOLD << "  | " << UL << "pop_bakc in loop:" << RESET << " vct.pop_back()" << std::endl;
+	for (int i = 0; i < 3;  i++)
+		vct.pop_back();
+	print_vector(vct);
 }
 
-int	ft_check_pushback_bis(void)
+void ft_check_pushback_bis(void)
 {
 	NS::vector<std::string> vct(8);
 	NS::vector<std::string> vct2;
@@ -283,10 +247,9 @@ int	ft_check_pushback_bis(void)
 
 	std::cout << "size : " << vct.size() << " | capacity : " << vct.capacity() << std::endl;
 	std::cout << "size : " << vct2.size() << " | capacity : " << vct2.capacity() << std::endl;
-	return (0);
 }
 
-void push_pop_back_tests(void)
+void pop_back_tests(void)
 {
 	std::cout << std::endl << "PUSH BACK & POP BACK TESTS" << std::endl;
 	NS::vector<int> test;
@@ -314,7 +277,7 @@ void push_pop_back_tests(void)
 	std::cout << " size " << test.size() << " | capacity : " << test.capacity() << std::endl;
 }
 
-void ft_check_insert()
+void ft_check_insert_ref()
 {
 	NS::vector<int> myvector (3,100);
 	NS::vector<int>::iterator it;
@@ -356,91 +319,41 @@ void ft_check_insert()
 	std::cout << '\n';
 }
 
-void ft_check_insert_bis()
+void ft_check_insert()
 {
-	std::cout << std::endl << "INSERT TESTS" << std::endl;
-	NS::vector<int> test(1, 1);
-	NS::vector<int> test2(5, 5);
-
-	test.insert(test.begin(), 200, 12);
-	std::cout << "size : " << test.size() << "  |  capacity : " << test.capacity() << std::endl;
-	test.insert(test.begin() + 12, 200, 30);
-	std::cout << "size : " << test.size() << "  |  capacity : " << test.capacity() << std::endl;
-	test.insert(test.end(), 10, 50);
-	std::cout << "size : " << test.size() << "  |  capacity : " << test.capacity() << std::endl;
-	test.insert(test.end() - 1, 0, 60);
-	std::cout << "size : " << test.size() << "  |  capacity : " << test.capacity() << std::endl;
-	test.insert(test.end() - 1, 1, 70);
-	std::cout << "size : " << test.size() << "  |  capacity : " << test.capacity() << std::endl;
-	test.insert(test.begin() + 412, test2.begin(), test2.end());
-	std::cout << "size : " << test.size() << "  |  capacity : " << test.capacity() << std::endl;
-	test.insert(test.begin() + 6, test2.begin(), test2.end());
-	std::cout << "size : " << test.size() << "  |  capacity : " << test.capacity() << std::endl;
-	test.insert(test.end(), test2.begin(), test2.end());
-	std::cout << "size : " << test.size() << "  |  capacity : " << test.capacity() << std::endl;
+	std::cout << PINK << ULL << "▶️​  INSERT TESTS " << RESET << std::endl;
+	NS::vector<int> vct(5, 5);
+	print_vector(vct);
+	std::cout << YELLDARK << BOLD << "  | " << UL << "insert(pos, value):" << RESET << " vct.insert(vct.end(), i * 7)" << std::endl;
+	for (int i = 0; i < 4; ++i)
+		vct.insert(vct.end(), i * 7);
+	print_vector(vct);
+	std::cout << YELLDARK << BOLD << "  | " << UL << "insert(pos, times, value):" << RESET << " vct.insert(vct.end() - 2, 3, 42)" << std::endl;
+	vct.insert(vct.end() - 2, 3, 42);
+	print_vector(vct);
+	NS::vector<int>::iterator it = vct.begin();
+	NS::vector<int> vct2(2,400);
+	std::cout << YELLDARK << BOLD << "  | " << UL << "insert(pos, iterator, iterator):" << RESET << " vct.insert(vct.end(), i * 7)" << std::endl;
+	vct.insert(it + 3,vct2.begin(),vct2.end());
+	print_vector(vct);
 }
 
 void ft_check_erase(void)
 {
-	NS::vector<int> second(5,100);
-	NS::vector<int>::iterator it1;
+	std::cout << PINK << ULL << "▶️​  ERASE TESTS " << RESET << std::endl;
+	NS::vector<int> vct(10, 5);
+	print_vector(vct);
+	std::cout << YELLDARK << BOLD << "  | " << UL << "erase(pos):" << RESET << " vct.erase(vct.begin())" << std::endl;
+	vct.erase(vct.begin());
+	print_vector(vct);
+	std::cout << YELLDARK << BOLD << "  | " << UL << "erase(pos) in loop:" << RESET << " vct.erase(vct.begin())" << std::endl;
+	for (int i = 0; i < 4; i++)
+		vct.erase(vct.begin());
+	print_vector(vct);
+	std::cout << YELLDARK << BOLD << "  | " << UL << "erase(pos, pos):" << RESET << " vct.erase(3)" << std::endl;
+	vct.erase(vct.begin() + 2, vct.end() - 2);
+	print_vector(vct);
 
-	second[0] = 1;
-	second[1] = 2;
-	second[2] = 3;
-	second[3] = 4;
-	second[4] = 5;
-	std::cout << "Vector : " << std::endl;
-	for (it1 = second.begin(); it1 < second.end(); it1++)
-		std::cout << ' ' << *it1;
-	std::cout << '\n';
-	std::cout << " ERASE : " << *second.begin()+2 << std::endl;
-	second.erase(second.begin()+2);
-	for (it1 = second.begin(); it1 < second.end(); it1++)
-		std::cout << ' ' << *it1;
-	std::cout << '\n';
-	second.erase(second.begin()+5);
-	for (it1 = second.begin(); it1 < second.end(); it1++)
-		std::cout << ' ' << *it1;
-	std::cout << '\n';
-
-	std::cout << "______________________________________" << std::endl;
-
-	NS::vector<int> first(10,20);
-	NS::vector<int>::iterator it;
-
-	for (int i = 0; i < 10; i++)
-		first[i] = i + 1;
-	std::cout << "Vector : " << std::endl;
-	for (it = first.begin(); it < first.end(); it++)
-		std::cout << ' ' << *it;
-	std::cout << '\n';
-	first.erase(first.begin()+2, first.begin() + 4);
-
-
-	for (it = first.begin(); it < first.end(); it++)
-		std::cout << ' ' << *it;
-	std::cout << '\n';
-	first.erase(first.begin()+10, first.begin()+20);
-	for (it = first.begin(); it < first.end(); it++)
-		std::cout << ' ' << *it;
-	std::cout << '\n';
-
-	std::cout << "REALITY " << std::endl;
-
-	NS::vector<int> real(10,20);
-	NS::vector<int>::iterator itr;
-
-	std::cout << "Vector : " << std::endl;
-	for (int i = 0; i < 10; i++)
-		real[i] = i + 1;
-	for (itr = real.begin(); itr < real.end(); itr++)
-		std::cout << ' ' << *itr;
-	std::cout << '\n';
-	real.erase(real.begin()+2, real.begin() + 4);
-	for (itr = real.begin(); itr < real.end(); itr++)
-		std::cout << ' ' << *itr;
-	std::cout << '\n';
 }
 
 void ft_check_erase_bis(void)
@@ -598,18 +511,17 @@ void vector_tests(void)
 
 	/**CAPACITY**/
 	std::cout << RED << BOLD << "------------- CAPACITY -------------" << RESET << std::endl << std::endl;
-
 		check_capacity();
 		// reserve_ref();
 		// check_resize_ref();
 
 	/**MODIFIERS**/
 	std::cout << RED << BOLD << "------------- MODIFIERS -------------" << RESET << std::endl << std::endl;
-
-		// ft_check_pushback();
+		ft_check_pushback();
 		// push_pop_back_tests();
 		// ft_check_pushback_bis();
-		// ft_check_insert();
+		// ft_check_insert_ref();
+		ft_check_insert();
 		// ft_check_erase();
 		// ft_check_erase_bis();
 		// std_check_erase_bis();
